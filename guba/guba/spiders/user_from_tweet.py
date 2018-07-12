81820e9b6f007f75cb8cdfb83b485468b5dcaddf
Kay Zhou <zkzhou_91@163.com>
2018-07-12 13:51:10 +0800

    update

---
+++ guba/guba/spiders/user_from_tweet.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import os
+import json
+import requests
+
+in_dir = "/home/kayzhou/Project/Guba_spider/guba/data"
+out_dir = "/home/kayzhou/Project/Guba_spider/guba/user_data"
+
+
+for in_name in os.listdir(in_dir):
+    in_name = os.path.join(in_dir, in_name)
+    with open(os.path.join(out_dir, in_name)) as f:
+        for line in open(in_name):
+            d = json.loads(line.strip())
+            url = d['url']
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, "lxml")
+            user_url = soup.select_one("#zwconttbn > strong > a")["href"]
+            uid = user_url.split('/')[-1]
+            del d["download_latency"]
+            del d["download_slot"]
+            del d["download_timeout"]
+            d['uid'] = uid
+            d['user_url'] = user_url
+            f.write(json.dumps(d, ensure_ascii=False) + '\n')
